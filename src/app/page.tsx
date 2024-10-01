b5872243f753110676ab4923ccf480b9495b8c7b
YoWuzzup <yowuzzup@live.com>
2024-10-01 12:28:36 +0200

    set up mongodb connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+// import clientPromise from "@/lib/mongodb";
+
+export default async function Home() {
+  // const client = await clientPromise;
+  // const db = client.db("npjb");
+  // const users = await db.collection("users").find({}).toArray();
+
   return (
     <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      asd
+
     </main>
   );
 }
