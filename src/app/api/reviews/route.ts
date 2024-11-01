84400d6595edc6aeca3447bb6ee8690eea5a1301
YoWuzzup <yowuzzup@live.com>
2024-11-01 11:49:16 +0100

    added api POST for adding new reviews

--- src/app/api/reviews/route.ts
+++ src/app/api/reviews/route.ts
@@ -3,6 +3,7 @@ import { Collection, Db, MongoClient } from "mongodb";
 
 import clientPromise from "@/lib/mongodb";
 import { TReview } from "@/components/componentTypes";
+import { generateId } from "@/lib/utils";
 
 export async function POST(request: Request) {
   try {
@@ -11,14 +12,35 @@ export async function POST(request: Request) {
     const db: Db = client.db("products");
     const reviews: Collection<TReview> = db.collection("reviews");
 
+    // data of review
     const data = await request.json();
-    console.log("Received data:", data);
 
-    return NextResponse.json("");
+    // generate and check public id
+    const publicId = async () => {
+      const id = generateId();
+
+      if (await reviews.findOne({ publicId: id })) {
+        return publicId();
+      }
+
+      return id;
+    };
+
+    const newReview = {
+      ...data,
+      date: new Date(),
+      publicId: await publicId(),
+      liked: 0,
+    };
+
+    // insert new review to the db
+    await reviews.insertOne(newReview);
+
+    return NextResponse.json({ success: true, review: newReview });
   } catch (error) {
-    console.error("Error fetching products: api/shop", error);
+    console.error("Error posting new review: api/reviews", error);
     return NextResponse.json(
-      { error: "Failed to fetch products" },
+      { error: "Failed to post a new review" },
       { status: 500 }
     );
   }
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,3 +5,16 @@ export const validateEmail = (email) => {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
 };
+
+export const generateId = (length = 10) => {
+  const timestamp = Date.now().toString(36);
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = timestamp;
+
+  for (let i = timestamp.length; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+};
